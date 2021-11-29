f66ea38bafd810ada8b54d1cdb91d6c397429ccd
Aleksandr <gmreloj@gmail.com>
2021-11-29 17:31:52 +0300

    nextStep

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         Warship warship1 = new Warship();
-        MerchantShip merchantShip = new MerchantShip();
+
+        Warship warship2 = new Warship();
+        
+
 
 
     }
--- src/main/java/gameObjects/vessels/Warship.java
+++ src/main/java/gameObjects/vessels/Warship.java
@@ -3,12 +3,26 @@ package gameObjects.vessels;
 public class Warship extends Vessel {
 
 
-    public Warship(){
+    public Warship() {
+        this.name="";
+        this.typeShip = "warship";
+        this.flagShip = "England";
+        this.maxSpeed=70;
+        this.currentSpeed=0;
+        this.maxGun = 24;
+        this.currentGun = 24;
+        this.maxCargoWeight = 600;
+        this.currentCargoWeight =400;
+        this.crew=60;
+        this.defender=0;
 
     }
-    public Warship(String name) {
-        this.name = name;
-    }
+
+
+
+
+
+
 
     public void repair() {
 
